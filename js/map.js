42a945a1360b153f56666cd0449ce428ee1e7e07
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-10-17 23:31:28 +0900

    3 maps

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var container = document.getElementById('map');
+var container = document.getElementById('map1', 'map2', 'map3');
 var options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
     level: 3
