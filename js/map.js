327826e41357802615abf1cd26c964abab85afd0
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-10-17 23:55:28 +0900

    maybe last maps

--- js/map.js
+++ js/map.js
@@ -1,23 +1,47 @@
 var container = document.getElementById('map1');
 var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(35.79236352987305, 127.42682652661878),
     level: 3
 };
 
 var map = new kakao.maps.Map(container, options);
 
+var markerPosition  = new kakao.maps.LatLng(35.79236352987305, 127.42682652661878); 
+
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker.setMap(map);
+
 var container2 = document.getElementById('map2');
 var options2 = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(35.98935452396864, 126.71108059764146),
     level: 3
 };
 
 var map = new kakao.maps.Map(container2, options2);
 
+var markerPosition2  = new kakao.maps.LatLng(35.98935452396864, 126.71108059764146); 
+
+var marker2 = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker2.setMap(map);
+
 var container3 = document.getElementById('map3');
 var options3 = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(35.85742410789543, 127.15551606880165),
     level: 3
 };
 
 var map = new kakao.maps.Map(container3, options3);
+
+var markerPosition3  = new kakao.maps.LatLng(35.85742410789543, 127.15551606880165); 
+
+var marker3 = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+marker3.setMap(map);
