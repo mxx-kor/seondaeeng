e54e79dddcae8800bbc2d144ce1169aea8954797
Minjae Kim <82329983+mxx-kor@users.noreply.github.com>
2021-10-17 23:34:21 +0900

    map check

--- js/map.js
+++ js/map.js
@@ -1,7 +1,15 @@
-var container = document.getElementById('map1', 'map2', 'map3');
+var container = document.getElementById('map1');
 var options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
     level: 3
 };
 
 var map = new kakao.maps.Map(container, options);
+
+var container2 = document.getElementById('map2');
+var options2 = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    level: 3
+};
+
+var map = new kakao.maps.Map(container2, options2);
